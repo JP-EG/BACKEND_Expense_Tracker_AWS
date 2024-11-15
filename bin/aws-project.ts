cadccc9b6f99626e075bb34badf79138a7839ea6
Jakub <jakub78b@gmail.com>
2024-11-15 19:43:25 +0000

    working through small fixes

--- bin/aws-project.ts
+++ bin/aws-project.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { AwsProjectStack } from '../lib/aws-project-stack';
+import { AwsProjectStack } from '../lib/Stack/aws-project-stack';
 
 const app = new cdk.App();
 
@@ -12,7 +12,6 @@ new AwsProjectStack(app, 'AwsProjectStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-    // env: { account: '010526243133', region: 'eu-west-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
